020a714b437e420d4a8c719583f873ce0ff97b24
Aasim Sayyad <55590434+aasim07@users.noreply.github.com>
2021-09-24 14:27:42 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -5,22 +5,10 @@ import pickle
 
 pickle_in = open("Random_forest_regressor.pkl","rb")
 random_forest_regressor=pickle.load(pickle_in)
-url = 'https://raw.githubusercontent.com/aasim07/Complete_Data_Science_Life_Cycle_Projects/master/Data_collection/Html_scraping_data/Real_combine.csv'
-df1 = pd.read_csv(url,index_col=0,parse_dates=[0])
 
-class_names=['27.9','31.5','20','1011.5','8.5']
 def predict(df):
-
-  df = df1.iloc[:,[0,1,2,3,6]]
-  
-  predictions=random_forest_regressor.predict(df)
+  df=df[['T','TM','Tm','SLP','H','VV','V','VM','PM 2.5']]
+  df1=df.iloc[:,[0,1,2,3,6]]
+  predictions=random_forest_regressor.predict(df1)[0]
+  print(predictions)
   return predictions
-
-  
-  ##return output
-
-def main():
-  predict(class_names)
-  
-if __name__=='__main__':
-    main()
