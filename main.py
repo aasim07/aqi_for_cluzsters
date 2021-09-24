14299b6f8ea1b4c1fa2b076ccba3c0e826373b55
Aasim Sayyad <55590434+aasim07@users.noreply.github.com>
2021-09-24 14:28:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,22 +6,19 @@ import pickle
 pickle_in = open("Random_forest_regressor.pkl","rb")
 random_forest_regressor=pickle.load(pickle_in)
 
-Average_Temperature= '27.9'
-Maximum_Temperature = '31.5'
-Minimum_Temperature = '20.6'
-Atm_pressure_at_sea_level = '1011.5'
-Average_wind_speed = '8.5'
-
-class_names=[Average_Temperature,Maximum_Temperature,Minimum_Temperature,Atm_pressure_at_sea_level,Average_wind_speed]
 def predict(df):
-
-  df = [ Average_Temperature,Maximum_Temperature,Minimum_Temperature, Atm_pressure_at_sea_level,Average_wind_speed]
-  predictions=random_forest_regressor.predict([[ Average_Temperature,Maximum_Temperature,Minimum_Temperature, Atm_pressure_at_sea_level,Average_wind_speed]])
+  df=df[['T','TM','Tm','SLP','H','VV','V','VM','PM 2.5']]
+  df1=df.iloc[:,[0,1,2,3,6]]
+  predictions=random_forest_regressor.predict(df1)[0]
   print(predictions)
   return predictions
- 
+
+  
+  ##return output
+
 def main():
-  predict(class_names)
+  combine_data= pd.read_csv(r'Real_combine.csv')
+  predict(combine_data)
   
 if __name__=='__main__':
     main()
